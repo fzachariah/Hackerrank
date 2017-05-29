f25dddcbfd777ff34da6e845610037582775a51f
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-05-29 12:19:44 -0400

    MoveZeros
    https://leetcode.com/problems/move-zeroes/#/description

---
+++ MoveZeros.java
@@ -0,0 +1,22 @@
+
+public class MoveZeros {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void moveZeroes(int[] nums) {
+		
+		int j = 0;
+	    for(int i = 0; i < nums.length; i++) {
+	        if(nums[i] != 0) {
+	            int temp = nums[j];
+	            nums[j] = nums[i];
+	            nums[i] = temp;
+	            j++;
+	        }
+	    }
+
+	}
+
+}
