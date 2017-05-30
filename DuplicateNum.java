82e7eef47023078010a5e9c1d1608ed34d90b41a
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-05-30 10:03:13 -0400

    DuplicateNum

--- DuplicateNum.java
+++ DuplicateNum.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class DuplicateNum {
@@ -7,25 +6,21 @@ public class DuplicateNum {
 		// TODO Auto-generated method stub
 
 	}
-	
-public boolean containsDuplicate(int[] nums) {
-        
-        HashSet<Integer> hashSet=new HashSet<>();
-        boolean check=false;
-        for(int i=0;i<nums.length;i++)
-        {
-        	if(!hashSet.contains(nums[i]))
-        	{
-        		hashSet.add(nums[i]);
-        	}
-        	else
-        	{
-        		check=true;
-        		break;
-        	}
-        }
-        return check;
-        
-    }
+
+	public boolean containsDuplicate(int[] nums) {
+
+		HashSet<Integer> hashSet = new HashSet<>();
+		boolean check = false;
+		for (int i = 0; i < nums.length; i++) {
+			if (!hashSet.contains(nums[i])) {
+				hashSet.add(nums[i]);
+			} else {
+				check = true;
+				break;
+			}
+		}
+		return check;
+
+	}
 
 }
